897588e2144f9d9f2915ff62d80cfaf3ce83d062
Drazen <drazenpetrovic01@gmail.com>
2026-02-10 20:59:59 +0100

    Izmjena kod greške Narudzba controller

--- src/controllers/aktivneNarudzbe.controller.js
+++ src/controllers/aktivneNarudzbe.controller.js
@@ -8,7 +8,20 @@ export const getAktivneNarudzbeGrupisano = async (req, res) => {
     const narudzbeGrupisane = await NarudzbeService.getAktivneNarudzbeGrupisano(req.sifraTerena); //Moguca greska
     return res.json({ success: true, data: narudzbeGrupisane, count: narudzbeGrupisane.length });
   } catch (error) {
-    console.error('Pregled artikala error:', error);
+    console.error('Pregled grupisanih narudžbi error:', error);
+    return res.status(500).json({ success: false, error: 'Greška pri učitavanju artikala' });
+  }
+};
+
+
+
+export const getAktivneNarudzbe = async (req, res) => {
+  try {
+    
+    const narudzbeGrupisane = await NarudzbeService.getAktivneNarudzbe(req.sifraTerena); //Moguca greska
+    return res.json({ success: true, data: narudzbeAktivne, count: narudzbeGrupisane.length });
+  } catch (error) {
+    console.error('Pregled narudžbi error:', error);
     return res.status(500).json({ success: false, error: 'Greška pri učitavanju artikala' });
   }
 };
