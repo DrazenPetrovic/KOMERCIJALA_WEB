72c8af54ace060c3b9ee000ecb88921dba1e8203
Drazen-Posao <drazenpetrovic01@gmail.com>
2026-06-08 09:31:55 +0200

    primjena produkvije

--- src/config/env.js
+++ src/config/env.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+dotenv.config({ path: `.env.${NODE_ENV}` });
 dotenv.config();
 
 export const env = {
   PORT: process.env.PORT || 3001,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  FRONTEND_URL: process.env.FRONTEND_URL || 'http://0.0.0.0:5173',
-  //FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173',
+  NODE_ENV,
+  FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173',
   JWT_SECRET: process.env.JWT_SECRET || 'change-me',
 };
